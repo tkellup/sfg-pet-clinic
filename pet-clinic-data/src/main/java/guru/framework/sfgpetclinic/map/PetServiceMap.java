eab8eacb9ea0a06da471ee27a6b7d16fa30a20dc
tkellup <tkellup@yahoo.com>
2021-01-22 12:10:39 -0600

    added a map based services. closes #6

---
+++ pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package guru.framework.sfgpetclinic.map;
+
+import guru.framework.sfgpetclinic.model.Pet;
+import guru.framework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+}
---
+++ pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package guru.framework.sfgpetclinic.map;
+
+import guru.framework.sfgpetclinic.model.Vet;
+import guru.framework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+}
